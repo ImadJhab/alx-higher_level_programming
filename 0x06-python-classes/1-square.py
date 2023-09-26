3636ea2c01d53c93e4497ca818d784771843190e
Imad Jhabli <imadjhabli@gmail.com>
2023-09-26 16:08:46 +0100

    stupid documentation

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""defines a square"""
+
+
+class Square:
+    """Square class"""
+    def __init__(self, size):
+        """Initialize a new instence of square
+        args:
+        size(int): size of the new square
+        """
+        self.__size = size
