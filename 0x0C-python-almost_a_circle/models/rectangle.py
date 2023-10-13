05d809a9e567449f22303e2cd763448ce61a1f43
Imad Jhabli <imadjhabli@gmail.com>
2023-10-13 15:46:09 +0100

    added width and height for the rectangle

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+from models.base import Base
+"""Defines Rectangle class"""
+
+
+class Rectangle(Base):
+    """initialize rectangle subclass
+    args:
+    width (int): width of the rectangle
+    height (int): height of the rectangle
+    x (int): coordinatge of the rectangle
+    y (int): coordinatge of the rectangle
+    id (int): id of the rectangle"""
+    def __init__(self, width, height, x=0, y=0, id=None):
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+        super().__init__(id)
+
+    @property
+    def width(self):
+        """set the width of the rectangle"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """self the height of the rectangle"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if type(value) != int:
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be > 0")
+        self.__height = value
