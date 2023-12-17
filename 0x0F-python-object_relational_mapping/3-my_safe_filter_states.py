389b282b53870e5b3082ea4d354a0d223b0d6eb5
Imad Jhabli <imadjhabli@gmail.com>
2023-12-17 15:32:29 +0100

    pain

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
                           passwd=argv[2], db=argv[3])
     cur = con.cursor()
     cur.execute("SELECT * FROM states WHERE name = %s ORDER BY states.id ASC",
-                (argv[4]))
+                (argv[4],))
     q_rows = cur.fetchall()
     for row in q_rows:
         print(row)
