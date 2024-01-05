9584c12edda2d9fcf5321982e3908218047f05cb
Imad Jhabli <imadjhabli@gmail.com>
2024-01-05 23:28:17 +0100

    watch true detective

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,22 +1,24 @@
 #!/usr/bin/python3
-""" Module that finds a peak in a list of unsorted integers """
+"""Finds a peak in a list of unsorted integers"""
 
 
 def find_peak(list_of_integers):
-    """ Function that finds a peak in a list of unsorted integers """
-    if not list_of_integers:
+    """Finds a peak in list_of_integers"""
+
+    if list_of_integers is None or list_of_integers == []:
         return None
-
-    def helper(start, end):
-        if start == end:
-            return list_of_integers[start]
-
-        mid = (start + end) // 2
-        if list_of_integers[mid] > list_of_integers[mid - 1] and list_of_integers[mid] > list_of_integers[mid + 1]:
-            return list_of_integers[mid]
-        elif list_of_integers[mid] < list_of_integers[mid - 1]:
-            return helper(start, mid - 1)
-        else:
-            return helper(mid + 1, end)
-
-    return helper(0, len(list_of_integers) - 1)
+    lo = 0
+    hi = len(list_of_integers)
+    mid = ((hi - lo) // 2) + lo
+    mid = int(mid)
+    if hi == 1:
+        return list_of_integers[0]
+    if hi == 2:
+        return max(list_of_integers)
+    if list_of_integers[mid] >= list_of_integers[mid - 1] and\
+            list_of_integers[mid] >= list_of_integers[mid + 1]:
+        return list_of_integers[mid]
+    if mid > 0 and list_of_integers[mid] < list_of_integers[mid + 1]:
+        return find_peak(list_of_integers[mid:])
+    if mid > 0 and list_of_integers[mid] < list_of_integers[mid - 1]:
+        return find_peak(list_of_integers[:mid])
